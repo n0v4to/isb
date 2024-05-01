35dddba717e54b14216581d7eb9c8e475824ca37
n0v4to <den.litvinov.94@list.ru>
2024-05-01 13:42:35 +0400

    add second NIST test

--- lab_2/pythonProject/NIST_tests.py
+++ lab_2/pythonProject/NIST_tests.py
@@ -4,6 +4,8 @@ import os
 
 from read_write_files import *
 
+PI = {0: 0.2148, 1: 0.3672, 2: 0.2305, 3: 0.1875}
+
 
 def frequency_test_nist(sequence: str, txt_file_path: str, key: str) -> None:
     """
@@ -25,18 +27,56 @@ def frequency_test_nist(sequence: str, txt_file_path: str, key: str) -> None:
     except Exception as e:
         print("Frequency bitwise test, ERROR: ", e)
 
+
 def same_bits_test_nist(sequence: str, txt_file_path: str, key: str) -> None:
     """
-            Perform frequency test based on NIST guidelines.
+        Perform frequency test based on NIST guidelines.
 
-            Parameters:
-            sequence (str): String of bits to be tested.
-            txt_file_path (str): Path to the text file where results will be written.
-            key (str): A key indicating which language the sequence was generated in.
+        Parameters:
+        sequence (str): String of bits to be tested.
+        txt_file_path (str): Path to the text file where results will be written.
+        key (str): A key indicating which language the sequence was generated in.
 
-            Returns:
-            None
+        Returns:
+        None
     """
+    try:
+        n = len(sequence)
+        ones_count = sequence.count("1")
+        share_of_unit = ones_count / n
+        if abs(share_of_unit - 0.5) < (2 / math.sqrt(n)):
+            v = 0
+            for bit in range(n - 1):
+                if sequence[bit] != sequence[bit + 1]:
+                    v += 1
+            numerator = abs(v - 2 * n * share_of_unit * (1 - share_of_unit))
+            denominator = 2 * math.sqrt(2 * n) * share_of_unit * (1 - share_of_unit)
+            p_v = math.erfc(numerator / denominator)
+        else:
+            p_v = 0
+        write_file(txt_file_path, f'{key} : {p_v}\n')
+    except Exception as e:
+        print("Test for the same consecutive bits, ERROR:", e)
+
+
+def longest_run_ones_test_nist(sequence: str, txt_file_path: str, key: str) -> None:
+    """
+        Perform frequency test based on NIST guidelines
+
+        Parameters:
+        sequence (str): String of bits to be tested.
+        txt_file_path (str): Path to the text file where results will be written.
+        key (str): A key indicating which language the sequence was generated in
+
+        Returns:
+        None
+    """
+    try:
+        n = len(sequence)
+        m = 8
+
+    except Exception as e:
+        print("Test for the longest sequence of ones in the block, ERROR:", e)
 
 
 if __name__ == "__main__":
@@ -44,7 +84,7 @@ if __name__ == "__main__":
     sequences = read_json(config["From"])
     sequences_cpp = sequences["C++"]
     sequences_java = sequences["Java"]
-    print(type(sequences_cpp))
-    print(type(sequences_java))
-    # frequency_test_nist(sequences_cpp, config["To"], "C++")
-    # frequency_test_nist(sequences_java, config["To"], "Java")
+    frequency_test_nist(sequences_cpp, config["To"], "C++")
+    frequency_test_nist(sequences_java, config["To"], "Java")
+    same_bits_test_nist(sequences_cpp, config["To"], "C++")
+    same_bits_test_nist(sequences_java, config["To"], "Java")
